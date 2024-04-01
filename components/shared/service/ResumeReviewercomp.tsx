d7de4278fe3efd1c118e70ea529282f4e2679163
void <95859137+voiiddxx@users.noreply.github.com>
2024-04-01 22:37:48 +0530

    Ui intiated for the resume reviwer component

--- components/shared/service/ResumeReviewercomp.tsx
+++ components/shared/service/ResumeReviewercomp.tsx
@@ -1,24 +1,36 @@
 "use client"
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 import DeskNav from '../landingpage/navigations/DeskNav';
 import { Button } from '@/components/ui/button';
 import Image from 'next/image';
 import { GoogleGenerativeAI } from '@google/generative-ai';
 import { ResumeReviewerProVision } from '@/lib/actions/pro-vision';
+import { ImageIcon, Send, X } from 'lucide-react';
 
 const ResumeReviewercomp = () => {
+
+
+    const ImageInputHanlder = useRef<any>(null);
+
+    const imageOpener = ()=>{
+        ImageInputHanlder.current.click();
+    }
+    
     const API_KEY = process.env.NEXT_PUBLIC_API_KEY;
     const genAI = new GoogleGenerativeAI(API_KEY!);
     const [ImageforSubmission, setImageforSubmission] = useState<any>(null);
 
-    const [file, setFile] = useState(null);
+    const [ImageForShow, setImageForShow] = useState<any>(null);
 
+    const [Prompt, setPrompt] = useState<string>('');
 
     const imageChangehandler = (event:any)=>{
         const file = event.target.files && event.target.files[0];
         if(file){
             const reader = new FileReader();
             reader.onloadend = ()=>{
+
+                setImageForShow(reader.result);
                 const base64ImageString = reader.result as string;
                const splittedImage =  base64ImageString.split(",")[1];
                 setImageforSubmission(splittedImage);
@@ -38,44 +50,69 @@ const ResumeReviewercomp = () => {
             console.log("Please upload the image of your resume");
         }
 
-       const data =  ResumeReviewerProVision({imageData:ImageforSubmission , prompt:"what is in this image"});
+       const data =  ResumeReviewerProVision({imageData:ImageforSubmission , prompt:Prompt});
     }
 
-    
-
-    const handleSubmit = async () => {
-        if (!ImageforSubmission) {
-            alert('Please select an image');
-            return;
-        }
-        try {
-            const model = genAI.getGenerativeModel({ model: 'gemini-pro-vision' });
-           
-
-            
-            
-            const result = await model.generateContent(["what is in this image?",  { inlineData: { data:ImageforSubmission , mimeType: 'image/jpeg' } }]);
-            const response = await result.response;
-            const text = await response.text();
-            console.log(text);
-        } catch (error) {
-            console.log(error);
-        }
-    };
 
     return (
-        <div className='min-h-screen w-full bg-black flex flex-col justify-between items-center'>
+        <div className='h-screen w-full bg-black flex flex-col justify-between items-center'>
             <div className='w-full'>
                 <DeskNav />
             </div>
             <div className='pb-8'>
-                <h1 className='text-indigo-400 text-2xl text-center'>Resume Reviewer</h1>
-                <p className='text-white font-light text-center mt-1'>Upload your resume and provide your job description</p>
-                <div>
+
+                
+               
+                <div className='flex w-full' >
+                    
+
+                    <div className='h-16 md:w-[800px] w-[400px] bg-zinc-900 rounded-full bg-opacity-70 flex items-center justify-between px-8 mb-16 md:mb-0' >
+                        <input onChange={(e)=>{
+                            setPrompt(e.target.value);
+                        }} type="text" placeholder='Enter Job Description...' className='bg-transparent placeholder:text-zinc-300 font-sans outline-none text-white w-[600px]' />
+
+
+            <input type="file" hidden  ref={ImageInputHanlder} onChange={imageChangehandler} />
+
+                        
+                       <div className='flex items-center gap-4' >
+                       {
+                        !ImageForShow && (
+                            <ImageIcon onClick={imageOpener} className='text-zinc-300' />
+                        )
+                       }
+                       {
+                    ImageForShow && (
+                        <div>
+                            <div className='relative' >
+                            <Image className='min-h-10 h-10 w-10 min-w-10 object-cover rounded-lg' src={ImageForShow} height={900} width={900} alt='thisisimage'/>
+                            <div className='h-4 w-4 rounded-full bg-zinc-900 absolute top-[-6px] right-[-5px] flex justify-center items-center ' >
+                                <X onClick={()=>{
+                                    setImageForShow(null);
+                                    setImageforSubmission(null);
+                                }} className='text-white' strokeWidth={1} size={12} />
+
+                            </div>
+                        </div>
+                        </div>
+                    )
+                }
+                       {
+                        Prompt !== '' && ImageforSubmission && (
+                            <Send onClick={handleVisionSubmit} className='text-blue-300' />
+                        )
+                       }
+                      
+                       </div>
+
+                       
+
+                    </div>
                     {/*  */}
-                    <input type="file"  onChange={imageChangehandler} />
-                    <input type="text" placeholder='enter job desc' />
-                    <Button onClick={handleVisionSubmit} className='bg-indigo-600 ml-4'>Generate</Button>
+                    
+                    {/* 
+                    <input type="text" placeholder='enter job desc' /> */}
+                    {/* <Button onClick={handleVisionSubmit} className='bg-indigo-600 ml-4'>Generate</Button> */}
                 </div>
             </div>
         </div>
--- lib/actions/pro-vision.ts
+++ lib/actions/pro-vision.ts
@@ -11,7 +11,7 @@ export const ResumeReviewerProVision =  async ({imageData , prompt}:ResumeReviwe
     const genai = new GoogleGenerativeAI(process.env.GEMINI_API_KEY!);
     const model = genai.getGenerativeModel({model:'gemini-pro-vision'});
     try {
-        const result = await model.generateContent(["what is in this image?",  { inlineData: { data:imageData , mimeType: 'image/jpeg' } }]);
+        const result = await model.generateContent([prompt,  { inlineData: { data:imageData , mimeType: 'image/jpeg' } }]);
             const response = await result.response;
             const text = await response.text();
             console.log(text);
